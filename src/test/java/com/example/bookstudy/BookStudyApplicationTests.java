6506653e949f2d6fdb00dca8122c5fd437b685ca
Donghyun <dh.kim3@okestro.com>
2023-12-06 17:41:53 +0900

    Initial commit

---
+++ src/test/java/com/example/bookstudy/BookStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
